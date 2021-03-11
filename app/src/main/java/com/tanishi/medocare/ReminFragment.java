389690cc952399ead1de8eb8977ab9176a0dc60f
Suverna Bisht <suvernabisht97@gmail.com>
2021-03-11 17:40:35 +0530

    prescription_and_reminder_fragments

---
+++ app/src/main/java/com/tanishi/medocare/ReminFragment.java
@@ -0,0 +1,22 @@
+package com.tanishi.medocare;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ReminFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.remin_frag, container, false);
+
+        return view;
+    }
+}
